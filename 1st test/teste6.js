029d4d0d11d0f8e5817912b190cdf65eec226916
Tiago Cruz <107953828+TiagoC18@users.noreply.github.com>
2023-03-10 20:28:01 +0000

    exercises

---
+++ 1st test/teste6.js
@@ -0,0 +1,45 @@
+﻿//--- TODO
+var quantidades = 0;
+var total = 0;
+var controlo=0
+
+function addProduct(number) {
+    numero = document.getElementById("price" + number).value;
+    qty = document.getElementById("qty" + number).value;
+    qty += 1;
+    total += parseFloat(numero);
+    quantidades += qty;
+    calculate()
+}
+
+function calculate() {
+    if (quantidades >= 5 || total > 100) {
+        var novototal = total * 0.95;
+        controlo = 1;
+    }
+    document.getElementById("").innerText(quantidades);
+    if (controlo == 1) {
+        document.getElementById("total").innerText(novototal);
+    } else {
+        document.getElementById("total").innerText(total);
+    }
+    
+}
+
+function valid() {
+    if (quantidades = 0) {
+        alert("Tem de adicionar pelo menos um produto");
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+function clean() {
+    document.getElementById("total").innerText("0");
+    document.getElementById("quantidades").innerText("0");
+    quantidades = 0;
+    total = 0;
+    controlo = 0;
+}
---
+++ Classes/aula07/calculadora.js
@@ -0,0 +1,92 @@
+﻿var start = true;
+var controlo = 1;
+var op1 = "";
+var op2 = "";
+var operacao = "";
+var res = document.getElementById("res")
+
+function addNumber() {
+    var x = event.target.value;
+    if (controlo == 1) {
+        op1 = op1 + x;
+    }
+    else {
+        op2 = op2 + x;
+    }
+    if (start == true) {
+        res.innerText = "";
+        start = false;
+    }
+    res.innerText += x
+}
+
+
+function clearResult() {
+    start = true;
+    controlo = 1;
+    op1 = "";
+    op2 = "";
+    operacao = "";
+    res.innerText = "0"
+}
+
+function addOperation() {
+    var x = event.target.value;
+
+    if (operacao =="" && op1 !="") {
+        operacao = x;
+        res.innerText += x;
+        controlo = 2;
+    }
+    else {
+        erro()
+    }
+
+}
+
+function erro() {
+    start = true;
+    controlo = 1;
+    op1 = "";
+    op2 = "";
+    operacao = "";
+    res.innerText = "Erro!!!";
+}
+
+function calculate() {
+    var error = 0;
+
+    var op1_numeric = parseInt(op1);
+    var op2_numeric = parseInt(op2);
+
+    if (controlo == 2 && op2 != "") {
+        switch (operacao) {
+            case "+":
+                op1 = op1_numeric + op2_numeric;
+                break;
+            case "-":
+                op1 = op1_numeric - op2_numeric;
+                break;
+            case "+":
+                op1 = op1_numeric * op2_numeric;
+                break;
+            case "/":
+                if (op2_numeric == 0) {
+                    erro();
+                    error = 1;
+                    break;
+                }
+                op1 = op1_numeric / op2_numeric;
+                break;
+        }
+        if (!error) {
+            controlo = 1;
+            op2 = "";
+            operacao = "";
+            res.innerText = op1;
+        }
+    }else {
+        erro()
+    }
+
+}
